1283e6e24a80440e194be7cfab8370a940955c89
Duy Tran <duy.tranquoc@gforces.auto>
2023-08-23 21:11:18 +0700

    [express] basic route setup with controller

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,20 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const taskRoutes = require('./routes/tasks');
+
+app.use(express.json()); // without this, we will not have data in req.body
+
+const logger = (req, res, next) => {
+    console.log(`hit server ${req.url}`);
+    next();
+}
+app.use([logger]);
+
+// routes
+app.get('/hello', (req, res) => {
+    res.send('Task manager App');
+});
+app.use('/api/v1/tasks', taskRoutes);
+
+const PORT = 4000;
+app.listen(PORT, console.log(`listening on port ${PORT}`));
